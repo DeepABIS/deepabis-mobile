e03490532ecfa19422da0329893380c1d108bb92
Keanu Buschbacher <keanu.buschbacher@gmail.com>
2018-07-23 00:01:27 +0200

    add species list fragment and prediction fragment skeleton

--- app/src/main/java/de/flynamic/mobileabis/MainActivity.kt
+++ app/src/main/java/de/flynamic/mobileabis/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.flynamic.mobileabis
 
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -10,20 +11,27 @@ import de.flynamic.mobileabis.inference.Labels
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SpeciesFragment.OnListFragmentInteractionListener, PredictFragment.OnFragmentInteractionListener {
+    private val speciesFragment = SpeciesFragment()
+    private val predictFragment = PredictFragment()
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onListFragmentInteraction(item: SpeciesFragment.SpeciesItem?) {
+
+    }
+
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_predict -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, predictFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_species -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, speciesFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
